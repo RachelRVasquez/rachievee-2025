2c5f36664b84a2e6aaf11ab8bb2d2b9e5665d589
unknown <rachelrvasquez@gmail.com>
2025-10-26 00:31:49 -0400

    adjusting widths and padding continued

--- patterns/footer.php
+++ patterns/footer.php
@@ -11,10 +11,9 @@
 <!-- /wp:html --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|70"},"blockGap":"0"}},"backgroundColor":"bg-footer","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-bg-footer-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","orientation":"horizontal","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"right","style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|70","right":"var:preset|spacing|40","left":"var:preset|spacing|40"},"blockGap":"0"}},"backgroundColor":"bg-footer","layout":{"type":"default"}} -->
+<div class="wp-block-group has-bg-footer-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"flex","flexWrap":"nowrap","orientation":"horizontal","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"align":"right","style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
 <p class="has-text-align-right" style="font-style:normal;font-weight:700"><?php esc_html_e('Source code available on Github', 'rachievee-2025');?></p>
 <!-- /wp:paragraph -->
 
@@ -23,6 +22,5 @@
 <!-- /wp:social-links --></div>
 <!-- /wp:group -->
 
-<!-- wp:navigation {"ref":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} /--></div>
-<!-- /wp:group --></div>
+<!-- wp:navigation {"ref":4,"overlayMenu":"never","style":{"typography":{"fontStyle":"normal","fontWeight":"600","textTransform":"uppercase"}},"fontSize":"sm","layout":{"type":"flex","justifyContent":"center"}} /--></div>
 <!-- /wp:group -->
